42486bf09a00adc3010f0d75e030a6ccb35947b2
user1 <user1@KH1>
2016-05-27 15:11:41 +0900

    124124

---
+++ wtg/src/admin/bean/UserstatsDTO.java
@@ -0,0 +1,50 @@
+package admin.bean;
+
+public class UserstatsDTO {
+	private String id;
+	private String year;
+	private String month;
+	private String day;
+	private String stats;
+	private String yemoda;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getStats() {
+		return stats;
+	}
+	public void setStats(String stats) {
+		this.stats = stats;
+	}
+	public String getYemoda() {
+		return yemoda;
+	}
+	public void setYemoda(String yemoda) {
+		this.yemoda = yemoda;
+	}
+	
+}
+
+
